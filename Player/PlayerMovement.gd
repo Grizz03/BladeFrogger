8ccac689b98e68445a8cae1c2f38c5e7ca3fe81f
grizz03 <sawilson1110@yahoo.com>
2021-05-16 22:00:24 -0700

    added movement to character

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -3,7 +3,14 @@ extends KinematicBody2D
 const speed = 200
 
 func _process(delta):
-    var velocity = Vector2.ZERO
-
-    Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
+	var velocity = Vector2.ZERO
+# Inputs for player movements
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	move_and_slide(velocity.normalized() * speed) # Normalized so x and y dont double speed
