9f00069b652c475ac79f08d69fcb7116026e9806
grizz03 <sawilson1110@yahoo.com>
2021-05-16 22:37:10 -0700

    added animations to movements

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -4,8 +4,10 @@ const speed = 200
 
 func _process(delta):
 	var velocity = Vector2.ZERO
+	
 # Inputs for player movements
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
+		$'AnimatedSprite'.scale.x = -1
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
@@ -14,3 +16,15 @@ func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 	move_and_slide(velocity.normalized() * speed) # Normalized so x and y dont double speed
+	player_animations(velocity)
+	
+func player_animations(velocity):
+	if velocity.y < 0:
+		$"AnimatedSprite".play('walk_up')
+	elif velocity.y > 0:
+		$'AnimatedSprite'.play('walk_down')
+	elif velocity.x != 0:
+		$'AnimatedSprite'.play('walk_side')
+		$'AnimatedSprite'.flip_h = velocity.x < 0
+	else:
+		$"AnimatedSprite".play('Idle')
